bc7866d66470eb830cddd9489270759ef0764a16
EricOliveira90 <ericfaria@gmail.com>
2022-05-26 19:05:45 -0300

    fix: better hero responsiveness

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,21 +7,25 @@ function Hero() {
   return (
     <header>
       <div className='top-space'></div>
-      <div className='center-space'>
-        <div className='focus-space'>
-          <img src={guru} alt="guru" className='guru'></img>
-          <div className='right-focus'>
-            <div className='slogan'>
-              <h1>OTIMIZE SEU JOGO COM A</h1>
-            </div>
-            <div className='logo'>
-              <img src={logo} alt="logo" className='guru-logo'></img>
-            </div>
-          </div>
-          
+      <div className='center-space'></div>
+      <div className='focus-space'>
+
+        <div className='guru'>
+          <img src={guru} alt="guru" width="300"></img>
         </div>
+        
+        <div className='right-focus'>
+          <div className='slogan'>
+            <h1>OTIMIZE SEU<br></br>JOGO COM A</h1>
+          </div>
+          <div className='logo'>
+            <img src={logo} alt="logo" className='guru-logo'></img>
+          </div>
+        </div>
+          
       </div>
-      <div className='top-space'></div>
+      teste
+      
     </header>
   )
 }
